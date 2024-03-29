258e75d27a529b19fbfb4c5da5a5ccd3af4e514c
ichichavez <isidrochavez429@gmail.com>
2024-03-29 11:10:05 -0400

    SideMenu-FirstIOSAPP

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+let myAge = 20
+let yourAge = 20
+
+if myAge > yourAge{
+    "I'm older than you"
+} else if myAge < yourAge{
+    "You older than me"
+} else {
+    "oh hey, we are the same age"
+}
+
+let myMothersAge = myAge + 35
+let doubleMyAge = myAge * 2
+
+let age = 30
+//let message: String
+//if age >= 18 {
+ //   message = "you are an adult"
+//}else {
+  //  message = "you ar not yet an adult"
+//}
+
+let message = age >= 18
+  ? "You are an adult"
+  : "you are not yet an adult"
+    
+
+
+
+
+
+
---
+++ variables.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+let myName = "ichi"
+
+var yourName = "Foo"
+
+var names = [
+    myName,
+    yourName
+]
+
+names.append("Bar")
+names.append("Baz")
